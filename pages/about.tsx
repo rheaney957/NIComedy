be1ba285d9f3596e833239106d09839732753541
Rhiannon Heaney <57272727+rheaney957@users.noreply.github.com>
2026-08-21 16:56:10 +0100

    Add files via upload

--- pages/about.tsx
+++ pages/about.tsx
@@ -24,13 +24,13 @@ export default function About({ menu, setMenu }: AboutProps)
       <main className={!menu ? styles.main : styles.mainMobile}>
         <Layout title="About Us" FAQs={true}>
           <section className={styles.about}>
-            <p>NI Comedy is Northern Ireland’s largest promoter of live comedy. Hosting tour shows in Belfast and regionally across Northern Ireland, we’ve hosted international acts such as KEVIN HART, TOM SEGURA, JOHN MULANEY, PAUL SMITH, DAVID O’DOHERTY, TIM DILLON, JIM JEFFERIES, JARLATH REGAN, LUCY BEAUMONT, PETE DAVIDSON, NEIL DELAMERE, JONATHAN VAN NESS & many others. </p>
+            <p>NI Comedy is Northern Ireland’s largest promoter of live comedy. Hosting tour shows in Belfast and regionally across Northern Ireland, we’ve hosted international acts such as KEVIN HART, TOM SEGURA, BILL BURR, JOHN MULANEY, PAUL SMITH, DAVID O’DOHERTY, TIM DILLON, JIM JEFFERIES, JARLATH REGAN, LUCY BEAUMONT, PETE DAVIDSON, NEIL DELAMERE, JONATHAN VAN NESS & many others. </p>
 
-            <p>We are also tour promoter for fantastic Irish acts such as SHANE TODD, COLIN GEDDIS, SERENA TERRY, DIONA DOHERTY, PADDY RAFF, PADDY MCDONNELL, EMER MAGUIRE, COLIN MURPHY & WILLIAM THOMPSON to name a few. </p>
+            <p>We are also tour promoter for fantastic Irish acts such as SHANE TODD, COLIN GEDDIS, SERENA TERRY, DIONA DOHERTY, PADDY RAFF, EMER MAGUIRE, COLIN MURPHY & WILLIAM THOMPSON to name a few. </p>
 
-            <p>We’ve presented numerous live podcast shows, such as TEA WITH ME, THE GUILTY FEMINIST, THE GIRLS BATHROOM, SAVING GRACE, THE BOMB SQUAD, HOW DID THIS GET MADE?, GENERAL BANTER, THE USELESS HOTLINE, THE GIRLS GROUP CHAT & more</p>
+            <p>We’ve presented numerous live podcast shows, such as TEA WITH ME, THE GUILTY FEMINIST, THE GIRLS BATHROOM, SAVING GRACE, THE BOMB SQUAD, HOW DID THIS GET MADE?, GENERAL BANTER, THE USELESS HOTLINE, THE GIRLS GROUP CHAT & more </p>
 
-            <p>We operate in venues across Northern Ireland, including Belfast's SSE Arena & Waterfront / Ulster Halls, Grand Opera House & Limelight venues, Derry’s Millennium Forum, and a huge amount of others across the country</p>
+            <p>We operate in venues across Northern Ireland, including The O2 Belfast, Waterfront / Ulster Halls, Grand Opera House & Limelight venues, Derry’s Millennium Forum, and a huge amount of others across the country. </p>
           </section>
         </Layout>
       </main >
